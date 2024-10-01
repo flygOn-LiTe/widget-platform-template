6eeea61ae39243396c0072c5264260085c7c0991
Andrew Rowberry <rowbs.js@gmail.com>
2024-09-30 18:49:15 -0700

    Update index.tsx

--- widget-platform-frontend/pages/index.tsx
+++ widget-platform-frontend/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
   return (
     <div className="h-screen flex justify-center items-center bg-gradient-to-b from-[rgba(151,0,173,1)] to-[rgba(46,5,64,1)]">
       <div className="flex flex-col items-center text-center">
@@ -20,7 +21,7 @@ export default function Home() {
         <h2 className="text-lg lg:text-5xl font-bold text-white pb-5 pt-5">
           A Twitch Widget Platform
         </h2>
-        <a href="https://widget-backend.xyz/auth/twitch">
+        <a href={`${backendUrl}/auth/twitch`}>
           <button className="btn">Login with Twitch</button>
         </a>
       </div>
