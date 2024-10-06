fc697732144e0bd700872cadd70a6a9b0e7019dc
Andrew Rowberry <rowbs.js@gmail.com>
2024-10-06 04:54:00 -0700

    Update index.tsx

--- widget-platform-frontend/pages/index.tsx
+++ widget-platform-frontend/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+  const backendUrl = `https://${process.env.NEXT_PUBLIC_BACKEND_URL}`;
   return (
     <div className="h-screen flex justify-center items-center bg-gradient-to-b from-[rgba(151,0,173,1)] to-[rgba(46,5,64,1)]">
       <div className="flex flex-col items-center text-center">
