4df0e613a993172b44cbf9c40cd9a9a3c5da88f8
Andrew Rowberry <rowbs.js@gmail.com>
2024-10-24 19:02:48 -0700

    Update _app.tsx

--- widget-platform-frontend/pages/_app.tsx
+++ widget-platform-frontend/pages/_app.tsx
@@ -9,9 +9,7 @@ const kanit = Kanit({
 });
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main
-      className={`${kanit.variable} font-sans bg-gradient-to-b from-[rgba(151,0,173,1)] to-[rgba(46,5,64,1)]`}
-    >
+    <main className={`${kanit.variable} font-sans`}>
       <Toaster />
       <Component {...pageProps} />
     </main>
