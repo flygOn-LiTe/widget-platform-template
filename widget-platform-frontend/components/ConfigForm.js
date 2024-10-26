cb8ce952e773f92e40a52721ce932a4efb9420d8
Andrew Rowberry <rowbs.js@gmail.com>
2024-10-25 18:57:22 -0700

    Update ConfigForm.js

--- widget-platform-frontend/components/ConfigForm.js
+++ widget-platform-frontend/components/ConfigForm.js
@@ -33,7 +33,7 @@ const ConfigForm = ({ onUpdate, widgetType }) => {
           Goal Amount:
         </label>
         <input
-          className="input input-bordered w-full p-2 rounded-md"
+          className="input input-bordered w-full max-w-md p-2 rounded-md"
           type="number"
           id="goal"
           name="goal"
@@ -51,13 +51,13 @@ const ConfigForm = ({ onUpdate, widgetType }) => {
           type="color"
           id="color"
           name="color"
-          className="w-full h-10 rounded-md"
+          className="w-full max-w-md h-10 rounded-md"
           value={color}
           onChange={(e) => setColor(e.target.value)}
         />
       </div>
 
-      <button className="btn btn-primary w-full" type="submit">
+      <button className="btn btn-primary w-full max-w-md" type="submit">
         Update
       </button>
     </form>
